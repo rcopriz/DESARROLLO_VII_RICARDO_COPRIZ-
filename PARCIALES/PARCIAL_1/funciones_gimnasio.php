6a1a58127005d1264548848efa04dc09f844f227
rcopriz <ricardo.copriz@utp.ac.pa>
2025-09-08 19:46:35 -0500

    primer avance problema 2

---
+++ PARCIALES/PARCIAL_1/funciones_gimnasio.php
@@ -0,0 +1,40 @@
+<?php
+    function calcular_promocion($antiguedad_meses)
+    {
+        if($antiguedad_meses < 3)
+        {
+            return "no hay promocion";
+        }
+
+        if($antiguedad_meses >= 3 && $antiguedad_meses <= 12)
+        {
+            return "8%";
+        }
+        if($antiguedad_meses >= 13 && $antiguedad_meses <= 24)
+        {
+            return "12%";
+        }
+        if($antiguedad_meses >= 24 )
+        {
+            return "20%";
+        }
+    }
+
+    function calcular_seguro_medico($cuota_base)
+    {
+        return $cuota_base * 0.05;
+    }
+
+    function calcular_cuota_final($cuota_base, $descuento_porcentaje, $seguro_medico)
+    {
+        return $cuota_base - ($cuota_base * ($descuento_porcentaje / 100)) + $seguro_medico;
+    }
+
+    /*
+    print(calcular_promocion(1) . "<br>");
+    print(calcular_promocion(5) . "<br>");
+    print(calcular_promocion(13) . "<br>");
+    print(calcular_promocion(25) . "<br>");
+    */
+
+?>
---
+++ PARCIALES/PARCIAL_1/gestionar_membresias.php
@@ -0,0 +1,7 @@
+<?php
+    $membresias = [ 'basica':'80', 'premium':'120','vip':'180','familiar':'250','corporativa':'300' ];
+    foreach ($membresias as $membresia)
+    {
+        print("<br> $membresia");
+    }
+?>
